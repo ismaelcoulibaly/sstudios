57010b7a0dc638f8c7eee0e680e0ab98fe62b1e9
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2021-09-18 13:58:12 -0400

    + and like tabs added + top action bar (to be changed bc not working properly)

--- app/src/main/java/com/sstudios/MainActivity.java
+++ app/src/main/java/com/sstudios/MainActivity.java
@@ -1,20 +1,16 @@
 package com.sstudios;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.sstudios.navigation.HomeFragment;
-import com.sstudios.navigation.MessagesFragment;
-import com.sstudios.navigation.SearchFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.sstudios.navigation.HomeFragment;
-import com.sstudios.navigation.MessagesFragment;
+import com.sstudios.navigation.AddFragment;
+import com.sstudios.navigation.LikesFragment;
 import com.sstudios.navigation.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView.add(new MeowBottomNavigation.Model(1, R.drawable.home_icon));
         bottomNavigationView.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_search_24));
-        bottomNavigationView.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_textsms_24));
+        bottomNavigationView.add(new MeowBottomNavigation.Model(3, R.drawable.ic_baseline_add_24));
+        bottomNavigationView.add(new MeowBottomNavigation.Model(4, R.drawable.ic_baseline_likes));
 
         bottomNavigationView.setOnShowListener(new MeowBottomNavigation.ShowListener() {
             @Override
@@ -48,19 +45,20 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case 3:
-                        selectedFragment = new MessagesFragment();
+                        selectedFragment = new AddFragment();
                         break;
+
+                    case 4:
+                        selectedFragment = new LikesFragment();
                 }
                 loadFragment(selectedFragment);
             }
         });
 
         //set notification count
-        bottomNavigationView.setCount(1, "10");
-        bottomNavigationView.setCount(3, "10");
-
+        bottomNavigationView.setCount(1, "7");
         bottomNavigationView.show(1, true);
-
+        bottomNavigationView.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), android.R.color.transparent));
         bottomNavigationView.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
             @Override
             public void onClickItem(MeowBottomNavigation.Model item) {
--- app/src/main/java/com/sstudios/navigation/LikesFragment.java
+++ app/src/main/java/com/sstudios/navigation/LikesFragment.java
@@ -12,10 +12,10 @@ import com.sstudios.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MessagesFragment#newInstance} factory method to
+ * Use the {@link LikesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MessagesFragment extends Fragment {
+public class LikesFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class MessagesFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public MessagesFragment() {
+    public LikesFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class MessagesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment MessagesFragment.
+     * @return A new instance of fragment LikesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MessagesFragment newInstance(String param1, String param2) {
-        MessagesFragment fragment = new MessagesFragment();
+    public static LikesFragment newInstance(String param1, String param2) {
+        LikesFragment fragment = new LikesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class MessagesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false);
+        return inflater.inflate(R.layout.fragment_likes, container, false);
     }
 }
